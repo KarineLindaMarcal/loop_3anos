861691c5afe0e81219cb67fcd17ab417c6c49ca3
k_maarcall <125562996+KarineLindaMarcal@users.noreply.github.com>
2024-04-16 12:16:49 -0300

    bbbb

--- main.js
+++ main.js
@@ -6,16 +6,19 @@ function verificarTime() {
   while (continuarPerguntando) {
     
     var respostaTime = prompt("Qual os melhor nail?");
-
-    
-    if (respostaTime.toLowerCase() === "Duda Nail") {
-      alert("Isso mesmo! El é a melhor de todas!");
+  
+  }
+  
+    if (respostaTime.toLowerCase() === "Duda Nail")
+    {
+      alert("Isso mesmo! Ela é a melhor de todas!");
       continuarPerguntando = false; // Sai do loop após a resposta correta
     } else {
       alert("Não é isso! Tente novamente.");
     }
   }
 }
+
 
 // Chamada da função para iniciar o processo
 verificarTime();
